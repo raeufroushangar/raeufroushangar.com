631d0da74ce7fb1afe2fe5aaf9df671a3e3db125
raeufroushangar <rroushangar19@gmail.com>
2024-07-20 00:09:56 +0000

    Updated CSS for date alignment and added JavaScript for email reveal

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+// assets/js/scripts.js
+document.getElementById("email-link").addEventListener("click", function() {
+    this.innerHTML = "<strong>email:</strong> rroushangar19@gmail.com";
+    this.style.textDecoration = "none";
+});
